f5b2f619dbf8cb3b92032252dde189072516bae4
ChanchalRajguru <rajguruchanchal@gmail.com>
2020-10-12 18:11:10 +0100

    Added Cors Mapping

---
+++ src/main/java/edu/ait/hospitalmanagement/config/WebMVCConfig.java
@@ -0,0 +1,18 @@
+package edu.ait.hospitalmanagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebMVCConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://localhost:3000")
+                .allowedMethods("GET", "PUT", "POST", "DELETE");
+    }
+}
