459746862ab32dbeb11840f4202526bdb5412f65
ChanchalRajguru <rajguruchanchal@gmail.com>
2020-10-15 21:21:17 +0100

    added wines get mapping

--- src/main/java/edu/ait/hospitalmanagement/controllers/loginController.java
+++ src/main/java/edu/ait/hospitalmanagement/controllers/loginController.java
@@ -1,19 +1,15 @@
 package edu.ait.hospitalmanagement.controllers;
 
-import edu.ait.hospitalmanagement.dto.HospitalUser;
 import edu.ait.hospitalmanagement.dto.Patient;
 import edu.ait.hospitalmanagement.repositories.HospitalUserRepository;
 import edu.ait.hospitalmanagement.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class loginController {
@@ -36,9 +32,10 @@ public class loginController {
         return ResponseEntity.created(location).build();
     }
 
-//    @GetMapping("wines")
-//    public Page<HospitalUser> getAllWines(Pageable pageable) {
-//        return hospitalUserRepository.findAll();
-//    }
+    //not working properly
+    @GetMapping("wines")
+    public List<HospitalUserRepository> getAllWines() {
+        return hospitalUserRepository.findAll();
+    }
 
 }
